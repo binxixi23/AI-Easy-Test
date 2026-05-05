c58d64f398a11966e7737afb31cc4a2960603b11
binxixi23 <binxixi23@gmail.com>
2026-05-05 16:04:45 -0500

    Create vision.py

---
+++ src/vision.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+class VesselDetector:
+    def get_vein_coordinates(self):
+        # Giả lập quét camera hồng ngoại
+        print("[AI Easy Test - Vision] Đang quét tìm tĩnh mạch...")
+        return {'x': 150, 'y': 80, 'z': 10}
